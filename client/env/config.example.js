75c3d76224a2af798ed4d15712f7ef58acaa5fcc
Janet Zheng <janet.zhengg@gmail.com>
2020-05-22 23:28:14 -0700

    id key added

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -3,9 +3,9 @@
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
+  jqXHR.setRequestHeader('X-Parse-Application-Id', 'Pf7b21f3a80d12fde9e86955b5f0f05b2bad808e0');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', '62d724a819f304c0e65d3015414127b322ac2706');
 });
 
 // Put your campus prefix here
-window.CAMPUS = 'FILL_ME_IN';
+window.CAMPUS = 'GHRSEA';
